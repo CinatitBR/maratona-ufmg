46f3c43d175d42510e345d9bc89d8d47f9e4c22e
Igor Rocha <cinatit.igor.santos@gmail.com>
2022-08-03 22:39:29 -0300

    Add __str__ method to models

--- problems/models.py
+++ problems/models.py
@@ -6,8 +6,14 @@ class Competitor(models.Model):
     user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
     institution = models.CharField(max_length=40)
 
+    def __str__(self):
+        return f'[{self.institution}] {self.user.username}'
+
 class Topic(models.Model):
     topic_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.topic_name
 
 class Problem(models.Model):
     class Difficulty(models.IntegerChoices):
@@ -19,11 +25,17 @@ class Problem(models.Model):
     url = models.URLField(max_length=500)
     difficulty = models.IntegerField(choices=Difficulty.choices)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    completed = models.ManyToManyField('auth.User')
+    completed = models.ManyToManyField('auth.User', blank=True)
+
+    def __str__(self):
+        return self.problem_name
 
 class Resource(models.Model):
     resource_name = models.CharField(max_length=255)
     url = models.URLField(max_length=500)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.resource_name
 
 # Easy, Medium, Hard
