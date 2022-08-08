b33a02b992d7903e63fedbf01cf2724db5f079a6
Igor Rocha <cinatit.igor.santos@gmail.com>
2022-08-08 13:16:39 -0300

    Create UserSerializer

--- problems/serializers.py
+++ problems/serializers.py
@@ -1,6 +1,12 @@
+from django.contrib.auth.models import User 
 from rest_framework import serializers
 from problems.models import Problem, Topic
 
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
 
 class ProblemSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
