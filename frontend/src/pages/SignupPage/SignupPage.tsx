39030dc3ea361777f9d1a4803efe77bc9bbb750d
Igor Rocha <cinatit.igor.santos@gmail.com>
2022-08-09 21:28:29 -0300

    Create SignupPage

---
+++ frontend/src/pages/SignupPage/SignupPage.tsx
@@ -0,0 +1,39 @@
+import AuthPageWrapper from '../../components/AuthPageWrapper/AuthPageWrapper'
+import LogoHeaderBox from '../../components/LogoHeaderBox/LogoHeaderBox'
+import ExternalButtonList from '../../components/ExternalButtonList/ExternalButtonList'
+import FormAuth from '../../components/FormAuth/FormAuth'
+import Field from '../../components/Field/Field'
+import Link from '../../components/Link/Link'
+
+const SignupPage = () => {
+  return <AuthPageWrapper>
+    <LogoHeaderBox text="Create an account" />
+
+    <ExternalButtonList />
+    
+    <FormAuth 
+      buttonText="Sign up"
+      switchLink={<Link href="#">Already have an account?</Link>}
+    >
+      <Field 
+        type="text" 
+        name="username" 
+        placeholder="Username"
+      />
+
+      <Field 
+        type="email" 
+        name="email" 
+        placeholder="Email"
+      />
+
+      <Field 
+        type="password" 
+        name="password" 
+        placeholder="Password"
+      />
+    </FormAuth>
+  </AuthPageWrapper>
+}
+
+export default SignupPage
